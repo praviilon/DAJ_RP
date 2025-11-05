ec0c14d081dcb7499807f1d5c657de8af04661cb
praviilon <104784237+praviilon@users.noreply.github.com>
2025-11-05 19:20:06 +0300

    fix login with spaces

--- codemp/ui/ui_main.c
+++ codemp/ui/ui_main.c
@@ -6356,7 +6356,7 @@ static void UI_RunMenuScript(char **args)
 					trap->Cvar_VariableStringBuffer("accLogin",zyk_login,sizeof(zyk_login));
 					trap->Cvar_VariableStringBuffer("accPassword",zyk_password,sizeof(zyk_password));
 
-					trap->Cmd_ExecuteText( EXEC_APPEND, va("new %s %s\n", zyk_login, zyk_password) );
+					trap->Cmd_ExecuteText( EXEC_APPEND, va("new %s %s\n", "zyk_login", "zyk_password") );
 				}
 				else if (Q_stricmp( "login", arg ) == 0)
 				{ // zyk: login the account
@@ -6366,7 +6366,7 @@ static void UI_RunMenuScript(char **args)
 					trap->Cvar_VariableStringBuffer("accLogin",zyk_login,sizeof(zyk_login));
 					trap->Cvar_VariableStringBuffer("accPassword",zyk_password,sizeof(zyk_password));
 
-					trap->Cmd_ExecuteText( EXEC_APPEND, va("login %s %s\n", zyk_login, zyk_password) );
+					trap->Cmd_ExecuteText( EXEC_APPEND, va("login %s %s\n", "zyk_login", "zyk_password") );
 				}
 				else if (Q_stricmp( "changepassword", arg ) == 0)
 				{ // zyk: change password of the account
@@ -6374,7 +6374,7 @@ static void UI_RunMenuScript(char **args)
 
 					trap->Cvar_VariableStringBuffer("accPassword",zyk_password,sizeof(zyk_password));
 
-					trap->Cmd_ExecuteText( EXEC_APPEND, va("changepassword %s\n", zyk_password) );
+					trap->Cmd_ExecuteText( EXEC_APPEND, va("changepassword %s\n", "zyk_password") );
 				}
 				else if (strstr(arg, "actionchange"))
 				{ // zyk: sets the action (upgrade or downgrade)
